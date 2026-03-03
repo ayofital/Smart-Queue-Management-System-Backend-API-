2860613d15581aa4786452b5dab90c9b63ca1246
Ayomidade <89843000+Ayomidade@users.noreply.github.com>
2026-03-03 10:22:44 +0100

    Update auth.controller.js
    updated the Login logic to use the user method to compare password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -69,7 +69,7 @@ export const loginUser = async (req, res) => {
     }
 
     // compare password
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await user.comparePassword(password)
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
