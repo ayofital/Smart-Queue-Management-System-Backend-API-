421a2b8d8996df03323738845b4854e4f8df21fd
ayofital <110603044+ayofital@users.noreply.github.com>
2026-03-04 08:10:18 +0100

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { protect, authorize } from "../middleware/auth.middleware.js";
 
-const userRouter = express.Router();
+const router = express.Router();
 
-userRouter.get("/profile", protect, (req, res) => {
+router.get("/profile", protect, (req, res) => {
   res.json(req.user);
 });
 
-userRouter.get("/admin-only", protect, authorize("admin"), (req, res) => {
+router.get("/admin-only", protect, authorize("admin"), (req, res) => {
   res.json({ message: "Welcome Admin" });
 });
 
-export default userRouter;
+export default router;
